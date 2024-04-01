7722a5f63b58882876f464c9d6e4b00fdc401919
Vinicius Hideaki Menda Nakano <vini.ttb@hotmail.com>
2024-04-01 16:48:10 -0300

    Add project files.

---
+++ udemy_SalarioFuncionario/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace udemy_SalarioFuncionario
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double salarioBruto;
+        public double imposto;
+
+        public double SalarioLiquido()
+        {
+            return salarioBruto - imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            porcentagem = porcentagem * 0.01;
+            porcentagem = porcentagem + 1;
+            salarioBruto = salarioBruto * porcentagem;
+        }
+    }
+}
---
+++ udemy_SalarioFuncionario/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace udemy_SalarioFuncionario
+{
+    /* Fazer um programa para ler os dados de um funcionário (nome, salario bruto e imposto). 
+     * Em seguida, mostrar os dados do funcionário (nome e salário líquido).
+     * Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada
+     * (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário.
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Digite as informações do funcionario: \n");
+
+            Console.WriteLine("Nome: ");
+            funcionario.nome = Console.ReadLine();
+            Console.WriteLine("Salario bruto: ");
+            funcionario.salarioBruto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Imposto: ");
+            funcionario.imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Nome: {funcionario.nome}\n" +
+                $"Salário: {funcionario.SalarioLiquido():n2}\n");
+
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Qual a porcentagem que deseja aumentar o salário?");
+            funcionario.AumentarSalario(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"Os novos valores são: \n" +
+                $"Funcionario: {funcionario.nome}\n" +
+                $"Salario: {funcionario.SalarioLiquido():n2}");
+        }
+    }
+}
